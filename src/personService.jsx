3bdf117da04e72b6d5b12bb2ec3c0e6351904f3f
Muchtar Salimov <muchtarsalimov@gmail.com>
2024-10-18 19:20:49 -0400

    add proxy and other changes related to joining with backend

--- src/personService.jsx
+++ src/personService.jsx
@@ -1,24 +1,24 @@
 import axios from "axios"
 
-const baseUrl = 'http://localhost:3001'
+const baseUrl = '/api/persons'
 
 const getAllPersons = () => {
-   const request = axios.get(`${baseUrl}/persons`)
+   const request = axios.get(`${baseUrl}`)
    return request.then((response) => response.data)
 }
 
 const createPerson = newPerson => {
-  const request = axios.post(`${baseUrl}/persons`, newPerson)
+  const request = axios.post(`${baseUrl}`, newPerson)
   return request.then((response) => response.data)
 }
 
 const updatePerson = (oldPerson, newPerson) => {
-  const request = axios.put(`${baseUrl}/persons/${oldPerson.id}`, newPerson)
+  const request = axios.put(`${baseUrl}/${oldPerson.id}`, newPerson)
   return request.then(response=>response.data)
 }
 
 const deletePerson = id => {
-  const request = axios.delete(`${baseUrl}/persons/${id}`)
+  const request = axios.delete(`${baseUrl}/${id}`)
   return request.then(response=>response.data)
 }
 
